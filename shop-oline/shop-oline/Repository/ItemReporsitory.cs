0fb93b39a8710acbf6a886c8dcd8ec445bae4994
Luis Daniel Martinez <luisdanielcurso@gmail.com>
2019-08-06 08:33:58 -0400

    Add the method ItemReporsitory

--- shop-oline/shop-oline/Repository/ItemReporsitory.cs
+++ shop-oline/shop-oline/Repository/ItemReporsitory.cs
@@ -9,6 +9,12 @@ namespace shop_oline.Repository
 
     public interface ItemReporsitory
     {
+        IEnumerable<Item> GetAll();
+        Item GetById(int Id);
+        void Insert(Item item);
+        void Update(Item item);
+        void Delete(int itemId);
 
+        void Save();
     }
 }
